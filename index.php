eaa7d503b17b6a93a2d93117b93bdd58d9e9f3bd
xuan <zhangpengxuan@yundun.com>
2016-09-13 11:56:38 +0800

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @node_name 导航权限节点描述
+ * Desc: 功能描述
+ * Created by PhpStorm.
+ * User: 张鹏玄 | <zhangpengxuan@yundun.com>
+ * Date: 2016/9/13 11:19
+ */
+require 'DemoService.php';
+$demoService = new \DemoService();
+$demoService->run();
